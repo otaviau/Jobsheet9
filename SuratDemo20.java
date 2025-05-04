220f07450c69f12b2a4bffee515d52ef8a7d3996
uotavia <uotavia@gmail.com>
2025-05-04 18:20:53 +0700

    update main latihan soal

--- SuratDemo20.java
+++ SuratDemo20.java
@@ -4,7 +4,7 @@ public class SuratDemo20 {
         Scanner sc = new Scanner(System.in);
         StackSurat stack = new StackSurat(10);
         int pilih;
-        
+
         do{
             System.out.println("\n------MENU------");
             System.out.println("1. Terima Surat Izin");
@@ -15,6 +15,43 @@ public class SuratDemo20 {
             System.out.print("Pilih: ");
             pilih = sc.nextInt();
             sc.nextLine();
+
+            switch (pilih) {
+                case 1:
+                    System.out.println("\n--TERIMA SURAT IZIN--");
+                    System.out.print("ID Surat:");
+                    String id = sc.nextLine();
+                    System.out.print("Nama Mahasiswa:");
+                    String nama = sc.nextLine();
+                    System.out.print("Kelas:");
+                    String kelas = sc.nextLine();
+                    System.out.print("Jenis Izin:");
+                    char jenis = sc.nextLine().charAt(0);
+                    System.out.print("Durasi:");
+                    int durasi = sc.nextInt(); sc.nextLine();
+                    stack.push(new Surat20(id, nama, kelas, jenis, durasi));
+                    break;
+                case 2:
+                    Surat20 diproses = stack.pop();
+                    if (diproses != null) {
+                        System.out.println("Memproses surat dari "+diproses.namaMhs);
+                    }
+                    break;
+                case 3:
+                    Surat20 terakhir = stack.peek();
+                    if (terakhir != null) {
+                        System.out.println("Surat terakhir dari "+terakhir.namaMhs);
+                    }
+                    break;
+                case 4:
+                    System.out.println("\n--CARI SURAT IZIN--");
+                    System.out.print("Masukkan nama mahasiswa :");
+                    String cari = sc.nextLine();
+                    stack.cariSurat(cari);
+                    break;
+                default:
+                    break;
+            }
         } while (pilih != 0);
     }
 }
